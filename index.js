ebbaf83f52292f9b0245b2ec3111832ad7c29384
Sakshi Chaudhary <scjuly69@gmail.com>
2022-02-11 22:51:35 +0530

    Feat: Added cart management and filters

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-const drawer = document.getElementById("drawer");
-const sideNavBtn = document.getElementById("toggle-side-menu");
-const sideDrawer = document.getElementById("drawer-menu");
-const closeSideDrawer = document.getElementById("close-side-menu");
+const openFilterBtn = document.getElementById("open-filter-menu");
+const filterMenu = document.getElementById("filter-menu");
+const closeFilterMenu = document.getElementById("close-filter-menu");
+const cartMenu=document.getElementById("cart-drawer-container");
+const openCartBtn=document.getElementById("open-cart");
+const closeCartBtn=document.getElementById("close-cart");
 
-/* Toggles side menu on mobile*/
-const toggleSideMenu = () => {
-  sideDrawer.classList.toggle("hide");
+/* Open filters*/
+const openFilter = () => {
+  filterMenu.classList.remove("hide");
+  document.body.style.overflow = 'hidden';
 };
 
-sideNavBtn.addEventListener("click", toggleSideMenu);
-closeSideDrawer.addEventListener("click", toggleSideMenu);
+/*Close filters */
+const closeFilter=()=>{
+  filterMenu.classList.add("hide");
+  document.body.style.overflow = 'unset';
+}
+
+/*Open cart*/
+const openCart=(e)=>{
+  e.preventDefault()
+  cartMenu.classList.remove("hide");
+  document.body.style.overflow = 'hidden';
+}
+/*Close cart */
+const closeCart=(e)=>{
+  e.preventDefault()
+  cartMenu.classList.add("hide");
+  document.body.style.overflow = 'unset';
+}
+
+openFilterBtn.addEventListener("click", openFilter);
+closeFilterMenu.addEventListener("click", closeFilter);
+openCartBtn.addEventListener("click",openCart);
+closeCartBtn.addEventListener("click",closeCart);
