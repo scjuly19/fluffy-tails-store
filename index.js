10e3a061099abeb81e6849c47bd63a3c439522d3
Sakshi Chaudhary <scjuly69@gmail.com>
2022-02-12 21:59:21 +0530

    Feat:Added login page UI

--- index.js
+++ index.js
@@ -1,36 +1,19 @@
-const openFilterBtn = document.getElementById("open-filter-menu");
-const filterMenu = document.getElementById("filter-menu");
-const closeFilterMenu = document.getElementById("close-filter-menu");
-const cartMenu=document.getElementById("cart-drawer-container");
-const openCartBtn=document.getElementById("open-cart");
-const closeCartBtn=document.getElementById("close-cart");
-
-/* Open filters*/
-const openFilter = () => {
-  filterMenu.classList.remove("hide");
-  document.body.style.overflow = 'hidden';
-};
-
-/*Close filters */
-const closeFilter=()=>{
-  filterMenu.classList.add("hide");
-  document.body.style.overflow = 'unset';
-}
+const cartMenu = document.getElementById("cart-drawer-container");
+const openCartBtn = document.getElementById("open-cart");
+const closeCartBtn = document.getElementById("close-cart");
 
 /*Open cart*/
-const openCart=(e)=>{
-  e.preventDefault()
+const openCart = (e) => {
+  e.preventDefault();
   cartMenu.classList.remove("hide");
-  document.body.style.overflow = 'hidden';
-}
+  document.body.style.overflow = "hidden";
+};
 /*Close cart */
-const closeCart=(e)=>{
-  e.preventDefault()
+const closeCart = (e) => {
+  e.preventDefault();
   cartMenu.classList.add("hide");
-  document.body.style.overflow = 'unset';
-}
+  document.body.style.overflow = "unset";
+};
 
-openFilterBtn.addEventListener("click", openFilter);
-closeFilterMenu.addEventListener("click", closeFilter);
-openCartBtn.addEventListener("click",openCart);
-closeCartBtn.addEventListener("click",closeCart);
+openCartBtn.addEventListener("click", openCart);
+closeCartBtn.addEventListener("click", closeCart);
